e5be6e599bc0e9c4c3334d5000c2ca4fdd44c605
Onur Erbilgin <onuruno.e@gmail.com>
2020-04-18 08:59:51 -0700

    REF: Create FanController class

--- pi-fanctrl/controller.py
+++ pi-fanctrl/controller.py
@@ -1,7 +1,6 @@
 """
 Controls fan speed based on CPU temperature.
 
-TODO: Make a class in gpiozero style
 TODO: Add multiple setpoints and interpolate between them
 TODO: Add logging and plotting function
 """
@@ -11,15 +10,33 @@ from time import sleep
 from gpio_ext import CPUTemperature
 
 
+class FanController():
+    def __init__(self, fan_pin, temp_lb=50, temp_ub=70):
+        self.fan = PWMOutputDevice(fan_pin)
+        self.temp_reader = CPUTemperature(
+            min_temp=temp_lb,
+            max_temp=temp_ub,
+            threshold=temp_ub)
+
+        self.fan.source = self.temp_reader
+
+    @property
+    def fan_speed(self):
+        return self.fan.value
+
+    @property
+    def cpu_temp(self):
+        return self.temp_reader.temperature
+
+    def start(self):
+        while True:
+            print(self.fan_speed, self.cpu_temp)
+            sleep(1)
+
+
 def main():
-    temp = CPUTemperature(min_temp=50, max_temp=70, threshold=70)
-
-    fan = PWMOutputDevice(17)
-    fan.source = temp
-
-    while True:
-        print(temp.temperature, fan.value)
-        sleep(1)
+    fan_control = FanController(17)
+    fan_control.start()
 
 
 if __name__ == '__main__':
